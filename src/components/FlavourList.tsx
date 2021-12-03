d8ad09f75412bda78628c7e6aa6935b02e264778
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-03 12:55:45 +0000

    Set up an initial order (none of everything)

--- src/components/FlavourList.tsx
+++ src/components/FlavourList.tsx
@@ -3,19 +3,43 @@ import { InventoryItem } from "../utils/InventoryItem";
 export function FlavourList(props: {
   inventory: InventoryItem[];
 }): JSX.Element {
+  // Setting up initial order with 0 for all flavours
+  let initialOrder: InventoryItem[] = [];
+  for (const item of props.inventory) {
+    initialOrder.push({
+      id: item.id,
+      name: item.name,
+      quantity: 0,
+    });
+  }
+  //   console.log(initialOrder);
+
+  // Create a table row for each flavour
   const FlavourListItem = (item: InventoryItem): JSX.Element => {
+    const flavourOrder = initialOrder.filter((obj) => obj.id === item.id)[0];
+    // console.log(flavourOrder);
     return (
-      <li key={item.id}>
-        {item.name[0].toUpperCase() + item.name.slice(1)}
-        {item.quantity === 0 && "(Out of stock)"}
-      </li>
+      <tr>
+        <td>
+          {item.name[0].toUpperCase() + item.name.slice(1)}
+          {item.quantity === 0 && <small> (out of stock)</small>}
+        </td>
+        <td>{flavourOrder.quantity}</td>
+      </tr>
     );
   };
 
   return (
     <>
-      <h2>Menu</h2>
-      <ul>{props.inventory.map(FlavourListItem)}</ul>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Menu</th>
+            <th scope="col">Quantity</th>
+          </tr>
+        </thead>
+        <tbody>{props.inventory.map(FlavourListItem)}</tbody>
+      </table>
     </>
   );
 }
