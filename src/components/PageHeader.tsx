3e4d1c8d21210936653843d347c0aa51157b54fc
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-03 12:21:21 +0000

    Initial commit

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,10 @@
+export function PageHeader(): JSX.Element {
+  return (
+    <header>
+      <h1>Welcome to Veta's Virtual Ice Cream Shop</h1>
+      <p>
+        Well, right now it's not really a "shop". All ice creams are free today!
+      </p>
+    </header>
+  );
+}
