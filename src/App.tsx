29fb2f3a91b006313c4037dc629686a25564b59f
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-03 12:05:36 +0000

    Fetch data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import "./css/App.css";
+import { useState, useEffect } from "react";
+import { InventoryItem } from "./utils/InventoryItem";
+import { fetchData } from "./utils/fetchData";
 
-function App() {
+function App(): JSX.Element {
+  const [inventoryData, setInventoryData] = useState<InventoryItem[]>([]);
+  useEffect(() => {
+    fetchData(setInventoryData);
+  }, []);
+  console.log(inventoryData);
   return (
     <>
       <h1>A simple hello world!</h1>
